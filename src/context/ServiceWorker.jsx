43f5a9666c7f0f7de947c27c9765a0af989f8c2e
anand <anandmohanjha241@gmail.com>
2025-09-14 17:03:46 +0530

    fixed service orker error

--- src/context/ServiceWorker.jsx
+++ src/context/ServiceWorker.jsx
@@ -6,7 +6,7 @@ export default function ServiceWorkerRegister() {
     if ("serviceWorker" in navigator) {
       window.addEventListener("load", () => {
         navigator.serviceWorker
-          .register("/service-worker.js")
+          .register("/sw.js")
           .then((reg) => console.log("Service worker registered:", reg.scope))
           .catch((err) => console.error("SW registration failed:", err));
       });
